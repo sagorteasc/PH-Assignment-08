d664b527a410f256a2d57466ad09c1c99b19b5e0
Md Naimul Islam Sagor <islamsagor49368@gmail.com>
2026-04-29 17:46:08 +0600

    initialize project

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@heroui/react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>Hello</Button>
+    </div>
+  );
+};
+
+export default Home;
